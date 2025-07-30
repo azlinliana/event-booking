9eec4cd3f2a8302c845b43bff6831d9ffef5f5d7
Nur Azlin Liana binti Mohd Adlan <azlinliana.adlan@gmail.com>
2025-07-30 19:04:21 +0800

    Update book controller
    1. Check no_purchased_ticket > total_ticket_available
    2. Store a newly booked ticket.
    3. Update total_ticket_available from the event list

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -4,13 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\Event;
 use App\Models\Ticket;
 use App\Models\Book;
 
 class BookController extends Controller
 {
     /**
-     * Store a newly created resource in storage.
+     * 1. Check no_purchased_ticket > total_ticket_available
+     * 2. Store a newly booked ticket.
+     * 3. Update total_ticket_available from the event list
      */
     public function store(Request $request)
     {
@@ -20,31 +23,58 @@ class BookController extends Controller
             'no_purchased_ticket' => ['required', 'integer'],
         ]);
 
-        // Get user info
-        $getUser = User::where('id', $request->user_id)
-            ->first();
+        // Get ticket
+        $getTicket = Ticket::find($request->ticket_id);
 
-        // Get ticket info
-        $getTicket = Ticket::where('id', $request->ticket_id)
-            ->first();
+        // Get current total_ticket_available from events
+        $getTotalTicketAvailable = $getTicket
+            ->event
+            ->total_ticket_available;
 
-        // Calculate total price of ticket based on the number purchased ticket
-        // ticket_price * no_purchased_ticket
-        $total_price_ticket = round(($getTicket->ticket_price) * ($request->no_purchased_ticket), 2);
+        // 1
+        if ($request->no_purchased_ticket > $getTotalTicketAvailable) {
+            return response()->json([
+                'message' => 'No. of purchased ticket is more than number of total available ticket.'
+            ], 500);
+        }
+        else {
+            // Get user info
+            $getUser = User::where('id', $request->user_id)
+                ->first();
 
-        // Create booking
-        $book = Book::create([
-            'user_id' => $getUser->id,
-            'ticket_id' => $getTicket->id,
-            'no_purchased_ticket' => $request->no_purchased_ticket,
-            'total_price_ticket' => $total_price_ticket
-        ]);
+            // Get ticket info
+            $getTicket = Ticket::where('id', $request->ticket_id)
+                ->first();
 
-        return response()->json([
-            'dataUser' => $getUser,
-            'dataTicket' => $getTicket,
-            'dataBooking' => $book,
-            'message' => 'Booking created!'
-        ], 200);
+            // Calculate total price of ticket based on the number purchased ticket
+            // ticket_price * no_purchased_ticket
+            $total_price_ticket = round(($getTicket->ticket_price) * ($request->no_purchased_ticket), 2);
+
+            // 2. 
+            // Create booking
+            $book = Book::create([
+                'user_id' => $getUser->id,
+                'ticket_id' => $getTicket->id,
+                'no_purchased_ticket' => $request->no_purchased_ticket,
+                'total_price_ticket' => $total_price_ticket
+            ]);
+
+            // 3
+            // Update total_ticket_available in the event list
+            $updateTotalTicketAvailable = $getTotalTicketAvailable - ($request->no_purchased_ticket);
+
+            // Get event info
+            $getEventInfo = $getTicket->event;
+
+            $getEventInfo->total_ticket_available = $updateTotalTicketAvailable;
+            $getEventInfo->save();
+
+            return response()->json([
+                'dataUser' => $getUser,
+                'dataTicket' => $getTicket,
+                'dataBooking' => $book,
+                'message' => 'Booking created!'
+            ], 200);
+        }
     }
 }
