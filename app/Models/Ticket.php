6ecedbcaaadce61078c2bacfb3b1fd2d715b41e3
Nur Azlin Liana binti Mohd Adlan <azlinliana.adlan@gmail.com>
2025-07-30 15:39:01 +0800

    Add relationship to ticket model
    Relationship to event & book model

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Ticket extends Model
 {
@@ -11,4 +13,20 @@ class Ticket extends Model
         'is_vip',
         'ticket_price'
     ];
+
+    /**
+     * Get the event that owns the ticket.
+     */
+    public function event(): BelongsTo
+    {
+        return $this->belongsTo(Event::class);
+    }
+
+    /**
+     * Get the booking for the ticket.
+     */
+    public function books(): HasMany
+    {
+        return $this->hasMany(Book::class);
+    }
 }
