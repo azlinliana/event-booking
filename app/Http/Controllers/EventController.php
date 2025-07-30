0e1825da42ba6bcf7e26bba34d10b0bdf3b0d705
Nur Azlin Liana binti Mohd Adlan <azlinliana.adlan@gmail.com>
2025-07-30 13:27:23 +0800

    Update store in event controller

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Event;
+use App\Models\Ticket;
 
 class EventController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Display a list of the event.
      */
     public function index()
     {
@@ -18,69 +19,38 @@ class EventController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
+     * Store a newly created event and ticket.
      */
     public function store(Request $request)
     {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
-            'date' => ['required'],
-            // 'venue' => ['required', 'string', 'max:255'],
+            'date' => ['required', 'date'],
+            'venue' => ['required', 'string', 'max:255'],
             'total_ticket_available' => ['required', 'integer'],
-            'ticket_price' => ['required'],
+            'is_vip' => ['required', 'boolean'],
+            'ticket_price' => ['required', 'numeric'],
         ]);
 
+        // Create event
         $event = Event::create([
             'name' => $request->name,
             'date'=> $request->date,
-            // 'venue'=> $request->venue,
+            'venue'=> $request->venue,
             'total_ticket_available'=> $request->total_ticket_available,
+        ]);
+
+        // Create ticket related to event
+        $ticket = Ticket::create([
+            'event_id' => $event->id,
+            'is_vip'=> $request->is_vip,
             'ticket_price' => $request->ticket_price,
         ]);
 
         return response()->json([
-            'data' => $event,
+            'dataEvent' => $event,
+            'dataTicket' => $ticket,
             'message' => 'Event created!'
         ], 200);
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
     }
 }
