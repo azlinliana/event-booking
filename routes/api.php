ea5d014391af3441d5a69c697a9c5af84d2aa00d
Nur Azlin Liana binti Mohd Adlan <azlinliana.adlan@gmail.com>
2025-07-30 13:23:35 +0800

    Update route api

--- routes/api.php
+++ routes/api.php
@@ -2,33 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-// use App\Http\Controllers\UserController;
 use App\Http\Controllers\EventController;
 use App\Http\Controllers\BookController;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
+// Event
+Route::get('/event', [EventController::class, 'index']); //List event
+Route::post('/event', [EventController::class, 'store']); // Event organizer create event
 
-// Route::prefix('auth')->group(function() {
-//     Route::get('/user', [UserController::class, 'index']);
+// Book
+Route::post('/book', [BookController::class, 'store']); // Customer & VIP book ticket
 
-//     Route::post('/user/register', [UserController::class, 'register']);
-
-//     Route::post('/user/login', [UserController::class, 'login']);
-
-//     Route::put('/user/{userId}', [UserController::class, 'update']);
-
-//     Route::delete('/user/{userId}', [UserController::class, 'detroy']);
-// });
-
-// Route::prefix('event')->group(function() {
-    Route::get('/event', [EventController::class, 'index']);
-
-    Route::post('/event', [EventController::class, 'store']);
-// });
-
-    Route::post('/book', [BookController::class, 'store']);
-
-
-
+// Ticket
+Route::get('/ticket', [TicketController::class, 'show']); // Customer & VIP view ticket
